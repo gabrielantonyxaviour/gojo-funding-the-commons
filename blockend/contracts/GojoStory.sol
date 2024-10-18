0bb97e98bbc76b8e10b08de72ef35d2f514c229e
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-10-18 01:07:31 -0700

    add testing layerzero

--- blockend/contracts/GojoStory.sol
+++ blockend/contracts/GojoStory.sol
@@ -2,32 +2,26 @@
 pragma solidity ^0.8.22;
 
 
-import { OApp, Origin, MessagingFee } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import { OApp, Origin, MessagingFee, MessagingReceipt} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-
-
 
 contract GojoStory is OApp{
        constructor(address _endpoint, address _owner) OApp(_endpoint, _owner) Ownable(_owner) {}
 
-    // Some arbitrary data you want to deliver to the destination chain!
     string public data;
+    
 
-    /**
-     * @notice Sends a message from the source to destination chain.
-     * @param _dstEid Destination chain's endpoint ID.
-     * @param _message The message to send.
-     * @param _options Message execution options (e.g., for sending gas to destination).
-     */
+    event MessageSent(bytes32 guid, uint32 dstEid, string message, MessagingFee fee, uint64 nonce);
+    event MessageReceived(bytes32 guid, uint32 srcId, bytes32 sender, address executor, string message, bytes extraData, uint64 nonce);
+   
     function send(
         uint32 _dstEid,
         string memory _message,
         bytes calldata _options
     ) external payable {
-        // Encodes the message before invoking _lzSend.
-        // Replace with whatever data you want to send!
+      
         bytes memory _payload = abi.encode(_message);
-        _lzSend(
+        MessagingReceipt memory _receipt = _lzSend(
             _dstEid,
             _payload,
             _options,
@@ -36,24 +30,17 @@ contract GojoStory is OApp{
             // Refund address in case of failed source message.
             payable(msg.sender)
         );
+        emit MessageSent(_receipt.guid, _dstEid, _message, _receipt.fee, _receipt.nonce);
     }
 
-    /**
-     * @dev Called when data is received from the protocol. It overrides the equivalent function in the parent contract.
-     * Protocol messages are defined as packets, comprised of the following parameters.
-     * @param _origin A struct containing information about where the packet came from.
-     * @param _guid A global unique identifier for tracking the packet.
-     * @param payload Encoded message.
-     */
     function _lzReceive(
         Origin calldata _origin,
         bytes32 _guid,
-        bytes calldata payload,
-        address,  // Executor address as specified by the OApp.
-        bytes calldata  // Any extra data or options to trigger on receipt.
+        bytes calldata _payload,
+        address _executor,  
+        bytes calldata _extraData  
     ) internal override {
-        // Decode the payload to get the message
-        // In this case, type is string, but depends on your encoding!
-        data = abi.decode(payload, (string));
+        data = abi.decode(_payload, (string));
+        emit MessageReceived(_guid, _origin.srcEid, _origin.sender, _executor, data, _extraData, _origin.nonce);
     }
 }
